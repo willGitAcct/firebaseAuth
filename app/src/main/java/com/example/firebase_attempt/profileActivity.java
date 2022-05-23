dda51bbe090be95aa2140efb9aa2ad18aed2177a
willGitAcct <willjhub@outlook.com>
2022-05-23 16:52:09 -0400

    added logout button and functionality to the user profile/login page

--- app/src/main/java/com/example/firebase_attempt/profileActivity.java
+++ app/src/main/java/com/example/firebase_attempt/profileActivity.java
@@ -2,13 +2,27 @@ package com.example.firebase_attempt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class profileActivity extends AppCompatActivity {
-
+    private Button logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        logout = (Button) findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(profileActivity.this, MainActivity.class));
+            }
+        });
     }
 }
